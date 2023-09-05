a5b559650112c701ebeeecaa8cc1096198d0b4fd
riycoders <you@example.com>
2023-09-05 20:24:04 +0700

    v3

---
+++ message/help.js
@@ -0,0 +1,118 @@
+const moment = require("moment-timezone");
+const fs = require("fs");
+
+moment.tz.setDefault("Asia/Jakarta").locale("id");
+
+let dt = moment(Date.now()).tz('Asia/Jakarta').locale('id').format('a')
+const ucapanWaktu = "Selamat "+dt.charAt(0).toUpperCase() + dt.slice(1)
+let setting = JSON.parse(fs.readFileSync('./config.json'))
+const { getLimit, getBalance, cekGLimit } = require("../lib/limit")
+
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+function toCommas(x) {
+	x = x.toString()
+	var pattern = /(-?\d+)(\d{3})/;
+     while (pattern.test(x))
+	   x = x.replace(pattern, "$1,$2");
+	return x;
+}
+
+exports.allmenu = (sender, prefix, pushname, isOwner, isPremium, balance, limit, limitCount, glimit, gcount) => {
+	return `${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
+
+Status : ${isOwner ? 'Owner' : isPremium ? 'Premium' : 'Free'}
+Limit Harian : ${isOwner ? '-' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
+Limit Game : ${isOwner ? '-' : cekGLimit(sender, gcount, glimit)}
+Balance : $${toCommas(getBalance(sender, balance))}
+
+*Main Menu*
+${prefix}menu
+${prefix}owner
+${prefix}donasi
+${prefix}speed
+${prefix}runtime
+${prefix}cekprem
+${prefix}listprem
+
+*Converter/Tools*
+${prefix}sticker
+${prefix}toimg
+${prefix}tovid
+${prefix}tomp3
+${prefix}ttp
+${prefix}emojimix
+${prefix}nulis
+${prefix}qc
+ 
+*Anonymous Chat*
+${prefix}anonymous
+${prefix}start
+${prefix}skip
+${prefix}stop
+${prefix}sendprofile
+${prefix}menfess
+
+*Downloader*
+${prefix}tiktok
+${prefix}instagram
+ 
+*Storage Menu*
+${prefix}addvn
+${prefix}listvn
+${prefix}getvn
+${prefix}delvn
+  
+*Random Menu*
+${prefix}quote
+${prefix}fakta
+${prefix}quoteanime
+${prefix}cecan
+${prefix}cogan
+${prefix}waifu
+  
+*Search Menu*
+${prefix}pinterest
+
+*Game Menu*
+${prefix}tebaklagu
+${prefix}family100
+${prefix}kuis
+${prefix}math
+${prefix}nyerah
+${prefix}tictactoe
+${prefix}delttt
+${prefix}casino
+${prefix}delcasino
+${prefix}bet
+  
+*Payment & Bank*
+${prefix}buylimit
+${prefix}buyglimit
+${prefix}transfer
+${prefix}limit
+${prefix}balance
+  
+*Group Menu*
+${prefix}linkgrup
+${prefix}setppgrup
+${prefix}setnamegc
+${prefix}setdesc
+${prefix}group
+${prefix}revoke
+${prefix}tagall
+${prefix}hidetag
+  
+*Owner Menu*
+> evalcode
+x evalcode-2
+$ executor
+${prefix}join
+${prefix}broadcast
+${prefix}setppbot
+${prefix}exif
+${prefix}leave
+${prefix}addprem
+${prefix}delprem`
+}
